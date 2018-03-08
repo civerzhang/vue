ab252175ee4660cd39f5893f33952eab9b8ef9c6
tdxzhangsiwei <934599659@qq.com>
2018-03-08 09:40:13 +0800

    同步

---
+++ my-project/src/components/my1.vue
@@ -0,0 +1,55 @@
+<template>
+<div id="calculator">
+    <textarea disabled="disabled" readonly="readonly"></textarea>
+    <div class="buttons">
+        <button value="1">1</button>
+        <button value="2">2</button>
+        <button value="3">3</button>
+        <button value="+">+</button>
+        <button value="4">4</button>
+        <button value="5">5</button>
+        <button value="6">6</button>
+        <button value="-">-</button>
+        <button value="7">7</button>
+        <button value="8">8</button>
+        <button value="9">9</button>
+        <button value="*">*</button>
+        <button value="clear">clear</button>
+        <button value="0">0</button>
+        <button value="=">=</button>
+        <button value="/">/</button>
+    </div>
+    <div id="history">
+
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'my1',
+  data () {
+    return {
+      msg: 'Welcome to sss Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import my1 from '@/components/my1'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/bar',
+      name: 'my1',
+      component: my1
     }
   ]
 })
